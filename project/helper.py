f286d11bc158227e6641457d5dc12268b2c5398a
zcrandall10 <zcrandall10@gmail.com>
2020-11-12 16:24:29 -0500

    comments

--- project/helper.py
+++ project/helper.py
@@ -1,5 +1,10 @@
+## notes
+## yp is previous (n-1)
+## h is step size
+## a is gamma
+## o is omega
 import numpy as np
-def midpoint(x, y, yp, f, h):
+def midp(x, y, yp, f, h):
     ## return next y
     return yp - 2*f(x, y)*h
 ## needed fo start midpoint
@@ -13,4 +18,10 @@ def f(x, y, a ,o):
 ## helper function
 def impMid(x, y, a, o, h):
     k = f(x, y, a, o)
-    return y + f(x+.5*h, y+.5*h*k, a, o)
+    return y + f(x+.5*h, y+.5*h*k, a, o)*h
+def midPoint(t, to, xo, xp0, steps, a, o):
+    stepsize = (t - to)/steps
+    y = [xo]
+    for i in steps:
+        y.append(yp - 2*f(x, y)*stepsize)
+    return
