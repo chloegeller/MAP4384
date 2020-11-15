d07a5d60333d7e4f73d1c4fbe44f47d4038fd1ea
zcrandall10 <zcrandall10@gmail.com>
2020-11-14 20:20:38 -0500

    fixed implicit and removed unused code

--- project/helper.py
+++ project/helper.py
@@ -5,42 +5,35 @@
 ## o is omega
 import numpy as np
 import math
-def midp(x, y, yp, h, a, o):
-    ## return next y
-    return yp - 2*f(x, y, a, o)*h
 ## needed fo start midpoint
-def eulers(x, y, a, o, h):
-    return y + h*f(x, y, a, o)
+def eulers(t, x, a, o, h):
+    return x + h*f(t, x, a, o)
 ## actual solution
 def x(t, a, b, x0):
     return x0*np.exp(-a*t)*np.cos(b*t)
 def f(x, y, a ,o):
     return -2*a*x-o*o*y
-## helper function
-def impMid(x, y, a, o, h):
-    k = f(x, y, a, o)
-    return y + f(x+.5*h, y+.5*h*k, a, o)*h
-def midPoint(t, to, xo, xp0, stepsize, a, o):
+def midPoint(t, to, xo, xp0, steps, a, o):
     y = [xo]
-    print(eulers(to,xo,a,o,stepsize))
-    print(x(stepsize, a, np.sqrt(3), xo))
-    y.append(eulers(to,xo,a,o,stepsize))
-    steps = int((t - to)/stepsize)
+    h = (t - to)/steps
+    y.append(eulers(to,xo,a,o,h))
     i = 0
-    for i in range(0, steps+1):
-        y.append(y[i] + 2*f(to + stepsize*(i+1), y[i+1], a, o)*stepsize)
+    for i in range(0, steps-1):
+        y.append(y[i] + 2*f(to + h*(i+1), y[i+1], a, o)*h)
     print(y[-1])
     return
-def implicitMidpoint(t, to, xo, xp0, stepsize, a, o):
-    y = [xo]
-    steps = int((t - to)/stepsize)
-    for i in range(0, steps):
-        k = f(to + i*stepsize, y[i], a, o)
-        y.append(y[i] + f(to+.5*stepsize, y[i]+.5*stepsize*k, a, o)*stepsize)
-    print(y[-1])
+def implicitMidpoint(t, to, xo, xp0, steps, a, o):
+    h = (t - to)/steps
+    y = np.zeros((steps+1,))
+    t = np.arange(to, t+h, h)
+    y[0] = xp0
+    for i in range(1, steps+1):
+        k = f(t[i-1], y[i-1], a, o)
+        y[i] = y[i-1] + f(t[i-1]+.5*h, y[i-1]+.5*h*k, a, o)*h
     return
+
 if __name__ == "__main__":
-    midPoint(5, 0, 1, 1, .0000001, 0, 2)
-    b = np.sqrt(4)
-    print(x(5, 0, b, 1))
-    implicitMidpoint(5, 0, 1, 1, .0000001, 0, 2)
+    # midPoint(1, 0, 1, 1, .001, 0, 2)
+    b = np.sqrt(4-1)
+    print(x(1, 1, b, 1))
+    implicitMidpoint(1, 0, 1, 1, 1000, 1, 2)
