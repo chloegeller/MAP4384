21daaf5872518257f56fe996117d7958bd900f20
zcrandall10 <zcrandall10@gmail.com>
2020-11-15 12:54:08 -0500

    changed to use IV

--- project/helper.py
+++ project/helper.py
@@ -6,36 +6,39 @@
 import numpy as np
 import math
 ## needed fo start midpoint
-def eulers(t, x, g, o, h):
-    return x + h*f(t, x, g, o)
+def eulers(t, x, xp0, g, o, h):
+    return xp0 + h*f(t, x, g, o)
 ## actual solution
 def x(t, g, b, x0):
     return x0*np.exp(-g*t)*np.cos(b*t)
 def f(x, y, g ,o):
     return -2*g*x-o*o*y
-def midPoint(t, t0, x0, xp0, steps, g, o):
+def midPoint(t, IV):
     t0, x0, xp0, steps, g, o = IV
     h = (t - t0)/steps
     y = np.zeros((steps+1,))
     t = np.arange(t0, t+h, h)
-    y[0] = eulers(t0,x0,g,o,h)
+    y[0] = eulers(t0, x0, xp0, g, o, h)
     for i in range(1, steps):
         y[i+1] = y[i-1] + 2*f(t[i], y[i], g, o)*h
     print(y[-1])
-    return
-def implicitMidpoint(t, t0, x0, xp0, steps, g, o):
+    return t, y
+def implicitMidpoint(t, IV):
+    t0, x0, xp0, steps, g, o = IV
     h = (t - t0)/steps
     y = np.zeros((steps+1,))
     t = np.arange(t0, t+h, h)
     y[0] = xp0
-    for i in range(1, steps+1):
+    for i in range(1, (steps+1)):
         k = f(t[i-1], y[i-1], g, o)
         y[i] = y[i-1] + f(t[i-1]+.5*h, y[i-1]+.5*h*k, g, o)*h
-    return
+    return t, y
 
 if __name__ == "__main__":
     # IV = (t0, x0, xp0, steps, g, o)
-    # midPoint(1, 0, 1, 1, .001, 0, 2)
+    IV =(0, 1, 1, 1000, 1, 2)
+    midPoint(1, IV)
     b = np.sqrt(4-1)
     print(x(1, 1, b, 1))
-    implicitMidpoint(1, 0, 1, 1, 1000, 1, 2)
+    implicitMidpoint(1, IV)
+    print(IV)
