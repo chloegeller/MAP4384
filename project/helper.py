f43cf1d954b366b2685549868300af41d1ee934d
zcrandall10 <zcrandall10@gmail.com>
2020-11-15 17:29:16 -0500

    ploting data to view behavior

--- project/helper.py
+++ project/helper.py
@@ -5,6 +5,7 @@
 ## o is omega
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 ## needed fo start midpoint
 def eulers(t, x, xp0, g, o, h):
     return xp0 + h*f(t, x, g, o)
@@ -21,7 +22,6 @@ def midPoint(t, IV):
     y[0] = eulers(t0, x0, xp0, g, o, h)
     for i in range(1, steps):
         y[i+1] = y[i-1] + 2*f(t[i], y[i], g, o)*h
-    print(y[-1])
     return t, y
 def implicitMidpoint(t, IV):
     t0, x0, xp0, steps, g, o = IV
@@ -33,12 +33,20 @@ def implicitMidpoint(t, IV):
         k = f(t[i-1], y[i-1], g, o)
         y[i] = y[i-1] + f(t[i-1]+.5*h, y[i-1]+.5*h*k, g, o)*h
     return t, y
-
+def exact(t, IV):
+    t0, x0, xp0, steps, g, o = IV
+    h = (t - t0)/steps
+    y = np.zeros((steps+1,))
+    t = np.arange(t0, t+h, h)
+    b = np.sqrt((o*o)-(g*g))
+    for i in range(0, (steps+1)):
+        y[i] = x0*np.exp(-g*t[i])*np.cos(b*t[i])
+    return t, y
 if __name__ == "__main__":
     # IV = (t0, x0, xp0, steps, g, o)
-    IV =(0, 1, 1, 1000, 1, 2)
-    midPoint(1, IV)
-    b = np.sqrt(4-1)
-    print(x(1, 1, b, 1))
-    implicitMidpoint(1, IV)
-    print(IV)
+    IV =(0, 1, 1, 2000, .1, .5)
+    tm, ym = midPoint(1, IV)
+    ti, yi = implicitMidpoint(3, IV)
+    tx, yx = exact(3,IV)
+    plt.plot(tx, yx, 'r', ti, yi, 'g')
+    plt.show()
