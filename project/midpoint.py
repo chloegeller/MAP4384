a9a32c44e0c8282149a833e224fefb6effd43a7e
zcrandall10 <zcrandall10@gmail.com>
2020-11-27 18:13:02 -0500

    Final update to methods

--- project/midpoint.py
+++ project/midpoint.py
@@ -9,10 +9,9 @@ from scipy.optimize import fsolve as fs
  #                                 #
 #####################################
 
-def midPoint(t, IV):
+def midPoint():
     y = np.zeros(steps+1)
     xl = np.zeros(steps+1)
-    t = np.arange(t0, t+h, h)
     xp0 = x0*gamma
     xl[0] = x0
     y[0] = xp0
@@ -23,7 +22,7 @@ def midPoint(t, IV):
         else:
             xl[i+1] = xl[i-1] + 2*h*y[i]
             y[i+1] = y[i-1] - 4*gamma*h*y[i] - 2*omega**2*h*xl[i]
-    return t, xl
+    return xl
 
 #####################################
  #                                 #
@@ -40,19 +39,16 @@ def IMP(p):
 
 def implicitMidpoint():
     
-    time=np.zeros(1)
     global x
     x = np.array([x0, xp0])
     traj=x
 
     for i in range(1, steps+1):
-        t=i*h
-        time=np.hstack([time,t])
         x_new = fs(IMP,x) 
         traj=np.vstack([traj,x_new])
         x=x_new
     
-    return time, traj
+    return traj
 
 #####################################
  #                                 #
@@ -69,19 +65,16 @@ def NSIMP(p):
 
 def nsImplicitMid():
     
-    time=np.zeros(1)
     global xn
     xn = np.array([x0, xp0])
     traj=xn
 
     for i in range(1, steps+1):
-        t=i*h
-        time=np.hstack([time,t])
         x_new = fs(NSIMP,xn) 
         traj=np.vstack([traj,x_new])
         xn=x_new
     
-    return time, traj
+    return traj
 
 #####################################
  #                                 #
@@ -89,15 +82,18 @@ def nsImplicitMid():
  #                                 #
 #####################################
 
-def exact(t, IV):
-    t0, x0, steps, g, o = IV
-    h = (t - t0)/steps
+def exact():
     y = np.zeros(steps+1)
-    t = np.arange(t0, t+h, h)
-    b = np.sqrt((o*o)-(g*g))
+    b = np.sqrt((omega**2)-(gamma**2))
     for i in range(0, (steps+1)):
-        y[i] = x0*np.exp(-g*t[i])*np.cos(b*t[i])
-    return t, y
+        y[i] = x0*np.exp(-gamma*t[i])*np.cos(b*t[i])
+    return y
+
+#####################################
+ #                                 #
+ #          Relative error         #
+ #                                 #
+#####################################
 
 def relativeError(exact, aprox, steps):
     rel = np.zeros((steps+1,))
@@ -106,35 +102,19 @@ def relativeError(exact, aprox, steps):
     return rel
 
 if __name__ == "__main__":
-    global t0
+    global t0, x0, steps, h, gamma, omega, xp0, t
     t0 = 0
-    global t
-    t = 1
-    global x0
+    tf = 1
     x0 = 1
-    global steps
     steps = 10
-    global h
-    h = (t -t0)/steps
-    global gamma
+    h = (tf - t0)/steps
     gamma = .1
-    global omega
     omega = 1
-    global xp0
     xp0 = x0*gamma
-
-    IV = (t0, x0, steps, gamma, omega)
-    #IV =(0, 1, 100, .0001, 1)
-    #tm, ym = midPoint(1, IV)
-    #ti, xi = implicitMidpoint()
-    #print(ti)
-    #print(xi)
-    tn, xn = nsImplicitMid()
-    print(tn)
-    print(xn)
-    tx, yx = exact(1,IV)
-    print(yx)
-    #plt.plot(tm, ym, 'r', ti, yi, 'g', tx, yx, 'b')
-    #plt.plot(ti, xi, 'r', tx, yx, 'g')
-    #plt.plot(tx, rel)
+    t = np.arange(t0, tf + h, h)
+    xm = midPoint()
+    xi = implicitMidpoint()
+    xn = nsImplicitMid()
+    xe = exact()
+    plt.plot(t, xe, 'g', t, xn[:,0], 'b', t, xi[:,0], 'r', t, xm, 'y')
     plt.show()
