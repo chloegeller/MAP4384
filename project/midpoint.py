f572c55ab9325d052502a5803a5c84814630744f
zcrandall10 <zcrandall10@gmail.com>
2020-12-07 19:41:46 -0500

    update midpoint.py to work like notebook

--- project/midpoint.py
+++ project/midpoint.py
@@ -1,7 +1,10 @@
 ## notes
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve as fs
+from tabulate import tabulate as table
+
 
 #####################################
  #                                 #
@@ -9,7 +12,7 @@ from scipy.optimize import fsolve as fs
  #                                 #
 #####################################
 
-def midPoint():
+def mid_point():
     y = np.zeros(steps+1)
     xl = np.zeros(steps+1)
     xp0 = x0*gamma
@@ -37,7 +40,7 @@ def IMP(p):
     deriv[1]= -2*gamma*(Y+x[1])/2 - omega**2*(X+x[0])/2 - (Y-x[1])/h
     return deriv
 
-def implicitMidpoint():
+def implicit_mid_point():
     
     global x
     x = np.array([x0, xp0])
@@ -63,7 +66,7 @@ def NSIMP(p):
     deriv[1]= - omega**2*(X+np.exp(-gamma*h)*xn[0])/2 - (Y-np.exp(-gamma*h)*xn[1])/h
     return deriv
 
-def nsImplicitMid():
+def ns_implicit_mid():
     
     global xn
     xn = np.array([x0, xp0])
@@ -95,26 +98,74 @@ def exact():
  #                                 #
 #####################################
 
-def relativeError(exact, aprox, steps):
+def relative_error(exact, aprox, steps):
     rel = np.zeros((steps+1,))
     for i in range(0, steps+1):
         rel[i] = abs(exact[i]-aprox[i])/abs(exact[i])
     return rel
 
-if __name__ == "__main__":
-    global t0, x0, steps, h, gamma, omega, xp0, t
+def results(t, x_m, x_i, x_n, err1, err2, err3, x_ex):
+    return list(zip(range(t.shape[0]), t, x_m, x_i, x_n, err1, err2, err3, x_ex))
+
+def run_test():
+    global t0, h, xp0, t
+
     t0 = 0
     tf = 1
-    x0 = 1
-    steps = 10
-    h = (tf - t0)/steps
-    gamma = .1
-    omega = 1
-    xp0 = x0*gamma
+    h = (tf - t0) / steps
+    xp0 = x0 * gamma
     t = np.arange(t0, tf + h, h)
-    xm = midPoint()
-    xi = implicitMidpoint()
-    xn = nsImplicitMid()
-    xe = exact()
-    plt.plot(t, xe, 'g', t, xn[:,0], 'b', t, xi[:,0], 'r', t, xm, 'y')
+
+    x_m = mid_point()
+    x_i = implicit_mid_point()
+    x_n = ns_implicit_mid()
+    x_ex = exact()
+
+    error_mid = relative_error(x_ex, x_m, steps)
+    error_imp = relative_error(x_ex, x_i[:,0], steps)
+    error_nsimp = relative_error(x_ex, x_n[:,0], steps)
+    error_exact = relative_error(x_ex, x_ex, steps)
+    
+    title1 = "Exact solution and methods: gamma=" + str(gamma) + ", omega=" + str(omega) + ", h=" + str(h)
+    plt.figure(figsize=(8, 6))
+    plt.plot(t, x_ex, "limegreen", label="Exact Solution", linewidth=5.5)
+    plt.plot(t, x_m, "cornflowerblue", label="EMP")
+    plt.plot(t, x_i[:,0], "blueviolet", marker="o", label="IMP")
+    plt.plot(t, x_n[:,0], "darkorange", label="NSIMP")
+    plt.xlabel("time")
+    plt.ylabel("x(t)")
+    plt.title(title1)
+    plt.legend(loc="upper right", fontsize="small")
+    plt.savefig("./plots/"+title1+".png")
     plt.show()
+    
+    title2 = "Errors: gamma=" + str(gamma) + ", omega=" + str(omega) + ", h=" + str(h)
+    plt.figure(figsize=(8,6))
+    plt.plot(t, error_exact, "limegreen", label="Exact Error", linewidth=5.5)
+    plt.plot(t, error_mid, "cornflowerblue", label="EMP Error")
+    plt.plot(t, error_imp, "blueviolet", marker="o", label="IMP Error")
+    plt.plot(t, error_nsimp, "darkorange", label="NSIMP Error")
+    plt.xlabel("time")
+    plt.ylabel("x(t)")
+    plt.title(title2)
+    plt.legend(loc="upper right", fontsize="small")
+    plt.savefig("./plots/"+title2+".png")
+    plt.show()
+    
+
+    print()
+    print(f"Testing using: steps = {steps}, x0 = {x0}, gamma = {gamma}, omega = {omega}, and h = {h}.", end="\n\n\n\n")
+    table_results = results(t, x_m, x_i[:,0], x_n[:,0], error_mid, error_imp, error_nsimp, x_ex)
+    print(table(table_results, headers=["n", "t", "EMP", "IMP", "NSIMP", "EMP Error", "IMP Error", "NSIMP Error", "Exact Solution"]))
+    print()
+    print()
+        
+    return
+
+if __name__ == "__main__":
+    global x0, steps, gamma, omega
+    steps = 10
+    x0 = 1
+    gamma = 0.1
+    omega = 1
+    run_test()
